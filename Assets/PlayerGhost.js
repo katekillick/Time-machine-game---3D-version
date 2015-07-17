0fba93d6986d53bdbb6a1d8d48a2a551e19ed5ce
katekillick <kate.killick@10to8.com>
2015-07-17 16:33:44 +0100

    Clean up, comments

--- Assets/PlayerGhost.js
+++ Assets/PlayerGhost.js
@@ -1,9 +1,9 @@
 ﻿#pragma strict
 
-public var ghostRecord : Array = new Array();
-var playingBack : boolean = false;
-public var startingPlayback : boolean = false;
-var ghostRoundCounter : int = 0;
+public var ghostRecord : Array = new Array(); //will store the recorded movements from the player
+var playingBack : boolean = false; //whether the ghost is currently in motion
+public var startingPlayback : boolean = false; //a trigger set off from the PlayerMovement.js script
+var ghostRoundCounter : int = 0; //counts which frame the ghost should load from the recorded movements
 
 function Start () {
 
@@ -11,15 +11,15 @@ function Start () {
 
 function Update () {
 
-	if (startingPlayback) { startPlayback(); }
+	if (startingPlayback) { startPlayback(); } //triggered from the PlayerMovement.js script
 
-	if (playingBack && ghostRoundCounter < ghostRecord.length) {
-    	transform.position = ghostRecord[ghostRoundCounter];
+	if (playingBack && ghostRoundCounter < ghostRecord.length) { //move the ghost around as long as it hasn't reached the end of it's recorded movements
+    	transform.position = ghostRecord[ghostRoundCounter]; //set the position accordingly
 	   	ghostRoundCounter++;
 	}
 }
 
-public function startPlayback() {
+public function startPlayback() { //start moving the ghost around
 	ghostRoundCounter = 0;
 	playingBack = true;
 	startingPlayback=false;
--- Assets/PlayerMovement.js
+++ Assets/PlayerMovement.js
@@ -5,29 +5,21 @@
  public var jumpDelay : float = 40;
  public var ghostPrefab : GameObject; 
  
- var recording : boolean = true;
- var startPosition : Vector3 = new Vector3(); //this will be where all the ghosts start
+ var recording : boolean = true; //decides whether we are recording player movements right now
+ var startPosition : Vector3 = new Vector3(); //where all the ghosts start
 
  
- var jumpCounter : int = 0;
- /*public var top_left : Transform;
- public var bottom_right : Transform;
- public var ground_layers : LayerMask;
- var grounded : boolean;*/
+ var jumpCounter : int = 0; //counts how long the player is in the air
+ var roundNumber : int = 0; //counts which "round" we are on - ie how many times the player has recorded movements and how many ghosts we therefore have
+ var roundCounter : int = 0; //counts the frames so far in this "round"
  
- var roundCounter : int = 0;
- var roundNumber : int = 0;
- 
- var playerRecord : Array = new Array();
- var ghosts = new GameObject[99]; //this will contain the list of ghost objects
- 
-//playerRecord = new Vector2[,];
+ var playerRecord : Array = new Array(); //will store the player's movements each round
+ var ghosts = new GameObject[99]; //will contain the list of ghost objects
  
  
  function Update() {
-    /* grounded = Physics2D.OverlapArea(top_left.GetComponent.<Transfposition, bottom_right.position, ground_layers);   */
-    
-    if (Input.GetKeyDown("a")) {
+ 
+    if (Input.GetKeyDown("a")) { //the input key for starting/ending recording the player movement
     	
     	if (recording) { 
     		stopRecording();
@@ -37,18 +29,19 @@
 		}
     }
     
-    if (recording) {
+    if (recording) { //if we are in the state of recording player movements...
     
+    	//let the player move around
 	    var move = Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 	    transform.position += move * speed * Time.deltaTime; 
 	    
+	    //let the player jump - v hacky right now!
 	    if (Input.GetKeyDown("space") && jumpCounter>jumpDelay) {
 		    GetComponent.<Rigidbody2D>().AddForce (new Vector2(0, jumpForce));
 		    jumpCounter=0;
 	    }
 	    
-	    jumpCounter++;
-	 //   Debug.Log(jumpCounter);
+	    jumpCounter++; //crappy jump hack
 	    
 	    //store the position of the player
 	    playerRecord[roundCounter] = transform.position;
@@ -56,94 +49,53 @@
 	    
     }
     
-    else {
-    	//transform.position = playerRecord[roundCounter];
-    	//roundCounter++;
-    	
-	   // if (roundCounter >= playerRecord.length) {
-		//    startRecording(); 
-	    //}
-	
+    else { //nothing here for now...
 	
     }
  }
  
- function startRecording() {
+ function startRecording() { //start recording player movements
 	 Debug.Log("start recording");
- 	 
 	 recording=true;
- 	
- 	
  }
  
- function stopRecording() {
+ function stopRecording() { //stop recording player movements and start ghosts playback
  
 	Debug.Log("stop recording");
  	recording = false;
- 	
- 	 //if this is after round 1, let's spawn a ghost based on the previous round..
- 	 //if (roundNumber>0) {
- 	 spawnGhost();
+
+
+ 	 spawnGhost(); //spawn a new ghost based on the player movements we've been recording
+  	 roundNumber++; //increase which round we are on
+ 	 startGhosts(); //set the ghosts in motion
  	 
-  	 roundNumber++;
- 	 
- 	 startGhosts();
- 	 //}
- 	 
-	 playerRecord.Clear();
+	 playerRecord.Clear(); //clear the record of the player's movements ready for the next round
  	
    	roundCounter=0;
  	
  }
  
- function spawnGhost() {
+ function spawnGhost() { //spawns a new ghost at the start of a round
  	//instantiate a new ghost object
  	var newGhost:GameObject = Instantiate(ghostPrefab, startPosition, Quaternion.identity);
  	
  	//assign the record of the round the player just played to this ghost
  	newGhost.GetComponent(PlayerGhost).ghostRecord = playerRecord.slice(0, playerRecord.length);	
  	
- 	
- //	Debug.Log(ghosts);
- 	ghosts[roundNumber] = newGhost;
+    //Debug.Log(ghosts);
+ 	ghosts[roundNumber] = newGhost; //add the new ghost into the ghost array
  }
  
- function startGhosts() {
- 	//send all the ghosts back to their starting position
- 	//set them all to be playing
- 	for (var i = 0; i<roundNumber; i++) {
+ function startGhosts() { //sets all the ghosts in motion
+ 
+ 	for (var i = 0; i<roundNumber; i++) { //loop through the ghosts
  		Debug.Log("ghost" + i + " is starting playback");
- 		ghosts[i].GetComponent(PlayerGhost).startingPlayback = true;
+ 		ghosts[i].GetComponent(PlayerGhost).startingPlayback = true; //this sets off a function in the PlayerGhost.js script
  	}
  }
  
  
  function Start() {
-	 startPosition = transform.position;
+	 startPosition = transform.position; //record the starting position for the rest of the game
  }
  
- /*
- function startRound() {
-	playerRecord[roundNumber] = Array;
- }
- 
- */
- /*
- #pragma strict 
-
- public var speed : float = 1.0;
- public var jumpSpeed : float = 1;
- var gravity : float = 20.0;
- 
- function Update() {
-	 var controller : CharacterController = GetComponent.<CharacterController>();
-     var move = Vector3(Input.GetAxis("Horizontal"), 0);
-     controller.SimpleMove(move * speed * Time.deltaTime);   
-     
-  if (controller.isGrounded && Input.GetKey ("up")) {
-  		var newMove = Vector3.up * jumpSpeed * Time.deltaTime;
-  		newMove.y -= gravity * Time.deltaTime;
-		controller.Move(newMove);
-	}
-		 
- }*/
